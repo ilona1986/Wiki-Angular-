43583999f9f0840377bafa8de7185e241abcbed4
Ilona <Ilona_b9ka@mail.ru>
2021-11-05 23:41:05 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JsonpComponent } from './jsonp/jsonp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JsonpComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/jsonp/jsonp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jsonp',
+  templateUrl: './jsonp.component.html',
+  styleUrls: ['./jsonp.component.scss']
+})
+export class JsonpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
